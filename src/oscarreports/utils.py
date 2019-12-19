1ba8ea14a2e4f75d6f6a98212f6caa18c49a1be3
Nigel Harsch <nharsch@thelabnyc.com>
2019-12-19 22:17:00 +0000

    Report repo register method

--- src/oscarreports/utils.py
+++ src/oscarreports/utils.py
@@ -1,5 +1,11 @@
 from oscar.apps.dashboard.reports import utils
+from django.core.exceptions import ImproperlyConfigured
 
 
 class GeneratorRepository(utils.GeneratorRepository):
-    pass
+
+    @classmethod
+    def register(cls, ReportGeneratorSubclass):
+        if ReportGeneratorSubclass in cls.generators:
+            raise ImproperlyConfigured('The class %s is already registered' % ReportGeneratorSubclass)
+        cls.generators.append(ReportGeneratorSubclass)
